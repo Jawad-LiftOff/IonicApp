14c4f43951aca6b2e204b6c87f7a7a5457352f23
Jawad-LiftOff <jawad@liftoffllc.com>
2015-07-09 13:09:50 +0530

    dates added

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,14 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 })
 
-.controller('EventCtrl', function ($scope, $ionicPopup) {
+.controller('EventCtrl', function ($scope, $ionicPopup) {  
   
-  $scope.today = new Date();
+  $scope.init = function () {
+    $scope.today = new Date();
+    $scope.bold = 'bold';
+    $scope.getWeek();
+  };
+  
   $scope.showPopup = function () {
     document.getElementsByTagName('ion-nav-view')[0].classList.add("doBlur");
     $scope.myPopup = $ionicPopup.show({
@@ -18,5 +23,20 @@ angular.module('starter.controllers', [])
     document.getElementsByTagName('ion-nav-view')[0].classList.remove("doBlur");
     $scope.myPopup.close();
   };
+  
+  $scope.getWeek = function () {
+    var days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+    var thisDay = $scope.today.getDay() == 0 ? 7 : $scope.today.getDay();
+    var daysToReduce = thisDay - 1;
+    var startDay = new Date(new Date().setDate($scope.today.getDate() - daysToReduce));    
+    $scope.daysInWeek = [];  
+    for (var i = 0; i < 7; i++) {
+      var day = new Date(new Date().setDate(startDay.getDate() + i)); 
+      day.day = days[i];     
+      $scope.daysInWeek.push(day);
+    }
+  };
+  
+  $scope.init();
 
 });
