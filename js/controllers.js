8e85ce20accf9d3936174d41889a22fe6bdb73e6
Jawad-LiftOff <jawad@liftoffllc.com>
2015-07-10 16:59:23 +0530

    code refactored. Services wired up

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,18 @@
 angular.module('dials.controllers', ['dials.services'])
 
-.controller('AppCtrl', function ($scope) {
+  .controller('AppCtrl', function ($scope) {
 })
 
-.controller('EventCtrl', function ($scope, $ionicPopup, EventsManager) {  
-  
+  .controller('EventCtrl', function ($scope, $ionicPopup, $filter, $timeout, DataManager) {
+
   $scope.init = function () {
     $scope.today = new Date();
-    $scope.bold = 'bold';   
+    $scope.bold = 'bold';
     $scope.setDate = new Date();
-    getEvents();    
+    getSchedule();
+    getEvents();
   };
-  
+
   $scope.showPopup = function () {
     document.getElementsByTagName('ion-nav-view')[0].classList.add("doBlur");
     $scope.myPopup = $ionicPopup.show({
@@ -21,43 +22,48 @@ angular.module('dials.controllers', ['dials.services'])
   };
 
   $scope.closePopup = function () {
-    document.getElementsByTagName('ion-nav-view')[0].classList.remove("doBlur");    
+    document.getElementsByTagName('ion-nav-view')[0].classList.remove("doBlur");
     $scope.myPopup.close();
   };
-  
+   
   $scope.getWeekData = function () {
-    var days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
-    var thisDay = $scope.today.getDay() == 0 ? 7 : $scope.today.getDay();    
+    var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    var thisDay = $scope.today.getDay() == 0 ? 7 : $scope.today.getDay();
     var weekStartDate = $scope.today.getDate() - (thisDay - 1);
     var weekStart = new Date(new Date().setDate(weekStartDate));
 
-    $scope.daysInWeek = [];  
-    for (var i = 0; i < 7; i++) {
-      var day = new Date(new Date().setDate(weekStart.getDate() + i)); 
-      day.day = days[i];  
-      day.hasEvent = $scope.eventDates.indexOf(day.getDate()) >= 0;   
+    $scope.daysInWeek = [];
+    _.each(days, function (d, i) {
+      var day = new Date(new Date().setDate(weekStart.getDate() + i));
+      day.day = d;
+      var date = (day.getUTCMonth() + 1) + '/' + day.getUTCDate() + '/' + day.getUTCFullYear();
+      var schedules = _.find($scope.schedule, function (data) { return new Date(data.date).toDateString() == new Date(date).toDateString() });
+      day.hasEvent = schedules;
       $scope.daysInWeek.push(day);
-    }
+    });
+
   };
 
-  var getEvents = function () {    
-    EventsManager.events(function (res) {      
-      $scope.eventDates = [];
-      for (var i = 0; i < res.length; i++) {              
-        var startDate = new Date(res[i].eventStartTime);  
-        res[i].date =  startDate.getDate();              
-        $scope.eventDates.push(startDate.getUTCDate());  
-      }  
-          
-      $scope.events = res;      
+  var getSchedule = function () {
+    DataManager.schedule(function (res) {
+      $scope.schedule = res;
       $scope.getWeekData();
     });
   };
-  
-  $scope.resetDate = function (day) {    
+
+  var getEvents = function () {
+    DataManager.events(function (res) {
+      _.each(res, function (data) {
+        data.date = new Date(data.start_time).getUTCDate();
+      });
+      $scope.events = res;
+    });
+  };
+
+  $scope.resetDate = function (day) {
     $scope.setDate = day;
   };
-  
+
   $scope.init();
 
 });
--- js/services.js
+++ js/services.js
@@ -1,12 +1,13 @@
 angular.module('dials.services', ['ngResource'])  
 
-.factory('EventsManager', function ($resource) {
-
-  // REST-API for the user is invoked from here
-  var service = $resource("data/:action/",
+.factory('DataManager', function ($resource) {
+  
+  var url = "http://private-fd322-oddball.apiary-mock.com";
+  var service = $resource(url + "/:action/",
     { action: '@action', id: '@id'},
     {
-      'events': {method: 'GET', isArray: true, params: {action: 'events.json'}}
+      'events': {method: 'GET', isArray: true, params: {action: 'events'}},
+      'schedule': {method: 'GET', isArray: true, params: {action: 'schedule'}}
     });
 
   return service;
